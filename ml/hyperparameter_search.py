73e2c826c4437660b0a5f9588fe66fc07489fee5
A. G. Polat <agplt@hotmail.com>
2016-02-11 16:27:50 +0200

    refactored config and construct

--- ml/hyperparameter_search.py
+++ ml/hyperparameter_search.py
@@ -28,9 +28,9 @@ def add_dense(model, hidden_layer_size, activation_function, dropout_rate, r):
         model.add(Dropout(dropout_rate))
 
 
-def random_construct_cnn(model, img_rows=28, img_cols=28, dense_limit=3, cnn_limit=4, nb_filters=42,
+def random_cnn_config(img_rows=28, img_cols=28, dense_limit=3, cnn_limit=4, nb_filters=42,
                          nb_pool=2,
-                         nb_conv=3, nb_classes=47, cnn_dropout_limit=0.1, dropout_limit=0.5, hidden_layer_limit=1024,
+                         nb_conv=3, nb_classes=47, cnn_dropout_limit=0.25, dropout_limit=0.75, hidden_layer_limit=1024,
                          border_modes=['same'], optimizers=['adadelta'],
                          loss_functions=['categorical_crossentropy'],
                          cnn_activation_functions=['relu'], dense_activation_functions=['relu'],
@@ -38,15 +38,12 @@ def random_construct_cnn(model, img_rows=28, img_cols=28, dense_limit=3, cnn_lim
                          ):
     border_mode = border_modes[random.randint(0, len(border_modes) - 1)]
     nb_conv = 2 + random.randint(0, nb_conv)
+    nb_filter = 10 + random.randint(0, nb_filters)
     activation_func = cnn_activation_functions[random.randint(0, len(cnn_activation_functions) - 1)]
     config = {"nb_conv": [nb_conv], "border_mode": border_mode, "img_rows": img_rows, "img_cols": img_cols,
               "nb_classes": nb_classes,
-              "nb_pool": [], "nb_filter": [], "dropout": [],
+              "nb_pool": [], "nb_filter": [nb_filter], "dropout": [],
               "activation": [activation_func], "dense_layer_size": [], "nb_repeat": []}
-    model.add(Convolution2D(nb_filters, nb_conv, nb_conv,
-                            border_mode=border_mode,
-                            input_shape=(1, img_rows, img_cols)))
-    model.add(Activation(activation_func))
     hard_limit = min(img_cols, img_rows)
     cnn_layer_n = 0
     for i in range(cnn_limit):
@@ -61,15 +58,12 @@ def random_construct_cnn(model, img_rows=28, img_cols=28, dense_limit=3, cnn_lim
         nb_filter = 10 + random.randint(0, nb_filters)
         layer_limit = random.randint(2, 6)
         r = 1 + random.randint(0, layer_limit - 1)
-        add_convolution(model, nb_filters, conv, pool, dropout_rate, activation_func, r)
         config["nb_repeat"].append(r)
         config["nb_conv"].append(conv)
         config["nb_pool"].append(pool)
         config["nb_filter"].append(nb_filter)
         config["dropout"].append(dropout_rate)
         config["activation"].append(activation_func)
-
-    model.add(Flatten())
     for i in range(dense_limit):
         if random.randint(0, 10) < 4:  # prevent from getting too big
             break
@@ -78,31 +72,31 @@ def random_construct_cnn(model, img_rows=28, img_cols=28, dense_limit=3, cnn_lim
         layer_limit = random.randint(2, 4)
         r = 1 + random.randint(0, layer_limit - 1)
         activation_func = dense_activation_functions[random.randint(0, len(dense_activation_functions) - 1)]
-        add_dense(model, hidden_layer_size, activation_func, dropout_rate, r)
         config["nb_repeat"].append(r)
         config["dense_layer_size"].append(hidden_layer_size)
         config["dropout"].append(dropout_rate)
         config["activation"].append(activation_func)
-
-    model.add(Dense(nb_classes))
-    model.add(Activation(final_activation))
     config["nb_classes"] = nb_classes
     config["activation"].append(final_activation)
     loss_function = loss_functions[random.randint(0, len(loss_functions) - 1)]
     optimizer = optimizers[random.randint(0, len(optimizers) - 1)]
     config["loss_function"] = loss_function
     config["optimizer"] = optimizer
-    model.compile(loss=loss_function, optimizer=optimizer)
     return config
 
+cnn_dict=random_cnn_config()
+print(cnn_dict)
 
-print(random_construct_cnn(Sequential()))
 
-
-def construct_cnn_from(dict_config):
-    """"nb_conv": [nb_conv], "border_mode": border_mode, "img_rows": img_rows, "img_cols": img_cols,
-              "nb_pool": [], "nb_filter": [], "dropout": [],
-              "activation": [activation_func], "dense_layer_size": [], "nb_repeat": []"""
+def construct_cnn(dict_config):
+    """"compile cnn from config dictionary
+    :param dict_config: contains the cnn hyperparameters
+    example config: {'optimizer': 'adadelta', 'nb_pool': [3, 2], 'activation': ['relu', 'relu', 'relu', 'softmax'],
+    'img_cols': 28, 'dropout': [0.07861946620850124, 0.01044751378147043], 'border_mode': 'same',
+    'dense_layer_size': [], 'nb_classes': 47, 'nb_filter': [26, 38], 'loss_function': 'categorical_crossentropy',
+    'nb_repeat': [3, 2], 'nb_conv': [5, 5, 6], 'img_rows': 28}
+    :return: keras model of type Sequential
+    """
     model = Sequential()
     nb_filters = dict_config["nb_filter"]
     border_mode = dict_config["border_mode"]
@@ -132,3 +126,4 @@ def construct_cnn_from(dict_config):
     model.add(Activation(activation_funcs[i]))
     model.compile(loss=dict_config["loss_function"], optimizer=dict_config["optimizer"])
     return model
+model= construct_cnn(cnn_dict)
