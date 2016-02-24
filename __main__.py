6e860cfef5865d634fe8a6680627a5e5bb8e0919
A. G. Polat <agplt@hotmail.com>
2016-02-24 15:27:18 +0200

    local search around promising configurations

--- __main__.py
+++ __main__.py
@@ -15,11 +15,11 @@ dict_config = {'bias_regularizers': [None, None, None, None], 'nb_pool': [3], 'n
                'sgd_lr_divide': 1.6892790823754182, 'dense_layer_size': [749, 879, 254, 864],
                'loss_function': 'categorical_crossentropy', 'nb_classes': 47, 'sgd_lr_init': 0.17006898785437682,
                'dense_inits': ['uniform', 'glorot_uniform', 'uniform', 'glorot_uniform'], 'sgd_nesterov': False,
-               'nb_filter': [32, 47], 'activation': ['linear', 'relu', 'linear', 'relu', 'relu', 'relu', 'softmax'],
+               'nb_filter': [32, 47], 'activation': ['linear', 'relu', 'linear', 'relu', 'relu', 'relu'],
                'nb_repeat': [2, 2, 2, 2, 1],
                'dropout': [0.05581567092442845, 0.246871561047429298, 0.0575979640541265788, 0.4836863105510306,
                            0.04954334377523167], 'dense_activity_regularizers': [None, None, None, None],
-               'img_rows': 28}
+               'img_rows': 28,'final_activation':'softmax'}
 meta = Pack()
 hyperparameter_search.search_near_promising(meta, trainer, dict_config, "48151")
 with open("data/dataset/meta_near_48151", "a") as meta_file:
