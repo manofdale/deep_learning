c53ec820f6c5d648f62c2046895b71d528f401ff
A. G. Polat <agplt@hotmail.com>
2016-03-04 11:00:42 +0200

    recover old weights 0.02

--- __main__.py
+++ __main__.py
@@ -40,7 +40,7 @@ dict_config2 = {'bias_regularizers': [None, None, None, None], 'nb_pool': [3], '
                             0.04954334377523167], 'dense_activity_regularizers': [None, None, None, None],
                 'img_rows': 28, 'final_activation': 'softmax'}
 meta = Pack()
-population_configs = [(0,41,dict_config0),(0.48151, dict_config2), (0.51916, dict_config)]
+population_configs = [(0.41, dict_config0), (0.48151, dict_config2), (0.51916, dict_config)]
 hyperparameter_search.search_around_promising(meta, trainer, population_configs, 0.4, "51916")
 print(population_configs)
 with open("data/dataset/meta_near_48151_incremental", "a") as meta_file:
